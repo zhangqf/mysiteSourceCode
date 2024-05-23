65a8f89a90355f526d6eb791315d52e2bfe68221
MAC <zhangfeng@zhangfengdeMacBook-Pro.local>
2024-05-23 18:18:51 +0800

    learn

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,24 +1,44 @@
 module.exports = {
-  title: 'Hello VitePress1',
+  title: '谦-后花园',
   base: '/mysite/',
   description: 'Just playing around.',
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/'},
-      { text: 'Example', link: 'markdown-examples'}
+      { text: 'Home', link: '/' },
+      { text: 'Example', link: '/张养浩' }
     ],
     sidebar: [
       {
         text: 'Example',
         items: [
-          { text: 'Markdown Example', link: '/markdown-examples'},
-          { text: 'Runtime API Example', link: '/api-example'}
+          { text: 'Markdown Example', link: '/markdown-examples' },
+          { text: 'Runtime API Example', link: '/api-example' }
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress'}
-    ]
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    },
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+
+    carbonAds: {
+      code: 'your-carbon-code',
+      placement: 'your-carbon-placement'
+    }
   }
 }
--- docs/index.md
+++ docs/index.md
@@ -24,7 +24,6 @@ features:
     title: Simple and minimal, always
     details: Lorem ipsum...
 ---
-
 <style>
 
     :root {
@@ -32,3 +31,19 @@ features:
   --vp-home-hero-name-background: -webkit-linear-gradient(120deg, #bd34fe, #41d1ff);
 }
 </style>
+
+# 山坡羊·一个犁牛半块田
+
+**元·张养浩**
+一个犁牛半块田，收也凭天，
+荒也凭天。粗茶淡饭饱三餐，
+早也香甜，晚也香甜。
+布衣得暖胜丝绵，长也可穿，
+短也可穿。草舍茅屋有几间，
+行也安然，待也安然。
+雨过天青驾小船，鱼在一边，
+酒在一边。夜归儿女话灯前，
+今也有言，古也有言。
+日上三竿我独眠，谁是神仙，
+我是神仙。南山空谷书一卷，
+疯也痴癫，狂也痴癫。
