e3597bd4e8a5399eca41dde0abad53e4faceb6f4
zhangqf <15893652937@163.com>
2024-06-22 01:01:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,11 +18,13 @@ git init  #执行这些git命令
 git add -A
 git commit -m 'deploy'
 
-eval $(ssh-agent -s)
-ssh-add ~/.ssh/id_rsa
+# eval $(ssh-agent -s)
+# ssh-add ~/.ssh/id_rsa
+echo $SSH_AGENT_PID
+echo $SSH_AUTH_SOCK
 
-# ssh-agent -a "$SSH_AUTH_SOCK"
-# ssh-add - <<< "$MYSITE"
+ssh-agent -a "$SSH_AUTH_SOCK"
+ssh-add - <<< "$MYSITE"
 
 git push -f git@github.com:zhangqf/mysite.git main:gh-pages  #提交到这个分支
 
