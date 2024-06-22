b4b7c3917b652bd690de38f3245c8a4d447fd4ab
zhangqf <15893652937@163.com>
2024-06-22 20:02:14 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,13 @@ set -e  #有错误抛出错误
 
 # 构建
 yarn run docs:build  #然后执行打包命令
-
 # 进入待发布的目录
 cd docs/.vitepress/dist  #进到dist目录
 git config --global init.defaultBranch main
 git init  #执行这些git命令
 
- git config --global user.email "15893652937@163.com"
- git config --global user.name "zhangqf"
+ #git config --global user.email "15893652937@163.com"
+ #git config --global user.name "zhangqf"
 
 
 git add -A
